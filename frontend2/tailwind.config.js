4c88a26e3503b8cc72d4cf8857dfd449ad066ed7
alimk754 <alimk27645@gmail.com>
2025-12-09 14:20:14 +0200

    removing files

--- frontend2/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],  // ← Empty array or remove the plugins property entirely
-}
