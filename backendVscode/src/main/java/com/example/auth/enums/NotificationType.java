5cb42fbfdd5455d1d64a76cdf1d489df04d36f0e
yaseenasaad12 <yaseenasaad12@gmail.com>
2025-12-20 22:52:49 +0200

    SL-147-"Implement Notification Backend Core with adding in database"

---
+++ backendVscode/src/main/java/com/example/auth/enums/NotificationType.java
@@ -0,0 +1,12 @@
+package com.example.auth.enums;
+
+public enum NotificationType {
+    CONNECTION_REQUEST,
+    CONNECTION_ACCEPTED,
+    POST_LIKE,
+    POST_COMMENT,
+    COMMENT_LIKE,
+    JOB_APPLICATION,
+    JOB_APPLICATION_STATUS_CHANGE
+}
+
