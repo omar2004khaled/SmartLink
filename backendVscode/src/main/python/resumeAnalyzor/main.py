fc3fd4d50e14c064ab41d8e56ca6883b49656586
Ali Eldeen <aliedeen17@gmail.com>
2025-12-23 23:05:24 +0200

    SL-156 hiding the api key to avoid leaking & adding the employee position parameter

--- backendVscode/src/main/python/resumeAnalyzor/main.py
+++ backendVscode/src/main/python/resumeAnalyzor/main.py
@@ -1,27 +1,33 @@
-from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi import FastAPI, UploadFile, File, HTTPException, Form
 from fastapi.middleware.cors import CORSMiddleware
 from services.textExtractor import extractText
 from services.cvAnalyzer import CVAnalyzer
 from models.analysisModels import AnalysisResult
+import os
+from dotenv import load_dotenv
+
+load_dotenv() 
 
 app = FastAPI(title="CV Analyzer API")
 app.add_middleware(
     CORSMiddleware,allow_origins=["*"],allow_credentials=True,allow_methods=["*"],allow_headers=["*"],
 )
 
-GEMINI_API_KEY = "AIzaSyC66Ue0VF-f9ddNMe1n5LFw_EMvGe9P_so"
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+if not GEMINI_API_KEY:
+    raise ValueError("GEMINI_API_KEY environment variable is required")
 cv_analyzer = CVAnalyzer(GEMINI_API_KEY)
 
 @app.post("/analyze-cv", response_model=AnalysisResult)
-async def analyze_cv(file: UploadFile = File(...)):
+async def analyze_cv(file: UploadFile = File(...), position: str = Form(...)):
     if not file.filename.endswith('.pdf'):
         raise HTTPException(status_code=400, detail="enter pdf file only")
     
     try:
-        cv_text = extractText(file)
-        if not cv_text.strip():
-            raise HTTPException(status_code=400, detail="No text found")
-        analysis = cv_analyzer.analyze_cv(cv_text)
+        cvText = extractText(file)
+        if not cvText.strip():
+            raise HTTPException(status_code=400,detail="No text found")
+        analysis = cv_analyzer.analyze_cv(cvText, position)
         return analysis
         
     except Exception as e:
