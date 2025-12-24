a48c0c8897a5b23045d2e52404ac66b9ede591bd
Ayman-Kotb <aymannkotb@gmail.com>
2025-12-24 19:37:06 +0200

    running tests

--- backendVscode/src/test/java/com/example/auth/controller/JobControllerTest.java
+++ backendVscode/src/test/java/com/example/auth/controller/JobControllerTest.java
@@ -14,6 +14,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -27,6 +29,8 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@AutoConfigureMockMvc
+@SpringBootTest
 public class JobControllerTest {
 
     private MockMvc mockMvc;
